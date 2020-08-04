1eb2d912c3953ac4a70323d2976ec6df6f0ccbb4
Kwothsei <45246543+Kwothsei@users.noreply.github.com>
2020-08-04 21:06:00 +0200

    Add project files.

---
+++ Controllers/SelfhostingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Quote_Website.Models;
+
+namespace Quote_Website.Controllers
+{
+    public class SelfhostingController : Controller
+    {
+        private readonly ILogger<SelfhostingController> _logger;
+
+        public SelfhostingController(ILogger<SelfhostingController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult index()
+            => View();
+
+        public IActionResult linux()
+            => View();
+
+        public IActionResult windows()
+            => View();
+
+        public IActionResult installer()
+            => View();
+
+        public IActionResult faq()
+            => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Quote_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
