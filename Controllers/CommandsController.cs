2930c629928a457ff11c8cbc026fab9cb6ba35de
Misaki <45246543+kageroukw@users.noreply.github.com>
2020-08-30 13:28:45 +0200

    Added a tab for commands, still WIP

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Quote_Website.Models;
+
+namespace Quote_Website.Controllers
+{
+    public class CommandsController : Controller
+    {
+        private readonly ILogger<CommandsController> _logger;
+
+        public CommandsController(ILogger<CommandsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult index()
+            => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
